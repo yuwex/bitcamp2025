62e90418c1169454a6cd4536d3daa698d36cf37f
Carson <CarsonWDalziel@gmail.com>
2025-04-13 01:59:30 -0400

    my stuff

--- scenes/station/station interaction/interact.gd
+++ scenes/station/station interaction/interact.gd
@@ -9,9 +9,11 @@ var interface
 
 func _on_area_entered(area: Area2D) -> void:
 	in_zone = true
+	print(in_zone)
 	
 func _on_area_exited(area: Area2D) -> void:
 	in_zone = false
+	print(in_zone)
 
 func _process(delta: float) -> void:
 	if in_zone and Input.is_action_just_pressed("interact") and has_event:
