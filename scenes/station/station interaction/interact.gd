fe9b796fb198d1d135e4a2aa57c195106e34b43f
Noah <156490730+nkasica@users.noreply.github.com>
2025-04-12 23:48:40 -0400

    stations

--- scenes/station/station interaction/interact.gd
+++ scenes/station/station interaction/interact.gd
@@ -1,13 +1,13 @@
-extends Sprite2D
+extends Area2D
 
-# for recieving events
-signal handle_events(events)
+var in_zone = false
 
-# pull up interfaces
-@export var interface_scene : PackedScene = load("res://scenes/station/event interface/eventinterface.tscn")
+func _on_area_entered(area: Area2D) -> void:
+	in_zone = true
+	
+func _on_area_exited(area: Area2D) -> void:
+	in_zone = false
 
-func _input(event):
-	if event.is_action_pressed("left-click"):
-		if is_pixel_opaque(get_local_mouse_position()):
-			var scene : EventInterface = interface_scene.instantiate()
-			scene.event = event
+func _process(delta: float) -> void:
+	if in_zone and Input.is_action_pressed("interact"):
+		print("handle signal")
